63e3540149968a072c252b89865a4a3470bb6bc2
juliosc1 <julio.siklander@gmail.com>
2023-03-02 19:46:39 +0100

    responsive navbar and menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,27 +12,41 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='flex justify-between items-center py-6 px-10'>
+    <div className='flex justify-between items-center py-6 px-10 z-50'>
       <div className='text-orange-500 flex justify-center items-center gap-2 text-xl font-semibold'>
         <h1 className=''>Pizza Place</h1>
         <FaPizzaSlice />
       </div>
 
-      <div className='flex justify-center items-center text-xl text-orange-500 gap-4'>
-        <ul className='flex justify-center items-center gap-4 font-semibold'>
+      <div className='flex justify-center items-center sm:text-xl text-orange-500 gap-4'>
+        <ul className='hidden md:flex justify-center items-center gap-4 font-semibold'>
           <li>Home</li>
           <li>Menu</li>
           <li>Specials</li>
           <li>Contatc Us</li>
         </ul>
 
-        <div className='flex border rounded-full border-orange-500 p-2'>
+        <div className='flex sm:hidden border-2 rounded-full border-orange-500 p-2'>
           <button onClick={handleClick}>
-            {click ? <IoCloseSharp /> : <HiMenuAlt3 /> }
+            {click ? <IoCloseSharp className='h-6 w-6' /> : <HiMenuAlt3 className='h-6 w-6' /> }
           </button>
         </div>
       </div>
     </div>
+    
+    {/* moblie */}
+    { 
+    <div className={click ? 
+    'fixed right-0 top-20 w-full h-full justify-center items-center border bg-orange-400 rounded-xl duration-500 ease-in md:hidden z-10' 
+    : "fixed -right-full top-20 h-full w-full duration-500 ease-out md:hidden z-10"}>
+      <ul onClick={() => setClick(false)} className='flex flex-col justify-center items-center text-white p-12 text-xl font-semibold w-full cursor-pointer'>
+        <li className='flex justify-center items-center py-4 border-b border-white w-full hover:scale-105'>Home</li>
+        <li className='flex justify-center items-center py-8 border-b border-white w-full hover:scale-105'>Menu</li>
+        <li className='flex justify-center items-center py-8 border-b border-white w-full hover:scale-105'>Special</li>
+        <li className='flex justify-center items-center py-8 border-b border-white w-full hover:scale-105'>Contact Us</li>
+      </ul>
+    </div>
+    }
     </>
   )
 }
